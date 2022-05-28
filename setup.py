617ba8c2ba835994b66ca8d9433db02424dd991f
Brénainn Woodsend <bwoodsend@gmail.com>
2022-05-28 14:07:28 +0100

    Apply pytest plugin coockiecutter template.

--- setup.py
+++ setup.py
@@ -8,15 +8,33 @@ readme = (HERE / "README.rst").read_text("utf-8")
 setup(
     author="Brénainn Woodsend",
     author_email="bwoodsend@gmail.com",
-    python_requires=">=3.6",
+    classifiers=[
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+    ],
     description="A pytest plugin to control the order in which tests are run in.",
+    entry_points={
+        "pytest11": ["ordered = pytest_pytest_ordered",],
+    },
     extras_require={
         "test": ["pytest>=3", "pytest-order", "coverage", "pytest-cov"]
     },
+    install_requires=["pytest>=3.5.0"],
     license="MIT",
     long_description=readme,
     name="pytest_ordered",
-    packages=find_packages(include=["pytest_ordered", "pytest_ordered.*"]),
-    url="https://github.com/bwoodsend/pytest_ordered",
+    py_modules=["pytest_ordered"],
+    python_requires=">=3.6",
     version="0.1.0",
+    url="https://github.com/bwoodsend/pytest_ordered",
 )
