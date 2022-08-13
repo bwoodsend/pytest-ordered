b954ea0b01c2e5860e175689b55e3cdfb14d3f55
Brénainn Woodsend <bwoodsend@gmail.com>
2022-08-13 17:29:08 +0100

    Bung it all in.

---
+++ pytest_ordered.py
@@ -0,0 +1,36 @@
+import re
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addini("order", "The order in which to run tests.", type="linelist")
+
+
+def pytest_collection_modifyitems(session, config, items):
+    if not config.getini("order"):
+        return
+    order = expand_test_order(config.inicfg["order"])
+    ranks = []
+    for test in items:
+        key = lambda item: test.location[0] == item[1]
+        best_match = max(enumerate(order), key=key)
+        if not key(best_match):
+            raise pytest.UsageError(
+                f'Test file "{test.location[0]}" does not match any of the '
+                f'test file names listed in the "order" section of the '
+                f'pytest.ini/setup.cfg file.'
+            )
+        ranks.append(best_match[0])
+    args = sorted(range(len(items)), key=ranks.__getitem__)
+    items[:] = [items[i] for i in args]
+
+
+def expand_test_order(x):
+    lines = re.findall("-( *)(.+)", x)
+    out = []
+    levels = {}
+    for _prefix, _name in lines:
+        levels[len(_prefix)] = _name
+        out.append("".join(j for (i, j) in levels.items() if i <= len(_prefix)))
+    return out
---
+++ tests/test_pytest_ordered.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import shutil
+
+import pytest_ordered
+
+
+def test_expand_test_order():
+    out = pytest_ordered.expand_test_order("""
+        - foo/
+        -     bar
+        -     baz_
+        -         pop
+        - lemon/cake
+    """)
+    assert out == ["foo/", "foo/bar", "foo/baz_", "foo/baz_pop", "lemon/cake"]
+
+
+MOCK_PROJECT = Path(__file__, "../../mock-project").resolve()
+
+
+def test_hello_ini_setting(pytester):
+    shutil.copytree(
+        MOCK_PROJECT / "tests",
+        pytester.path / "tests",
+        ignore=lambda _, names: [i for i in names if i == "__pycache__"])
+
+    pytester.makefile(".ini", pytest="")
+    result = pytester.runpytest('-v', "-W", "error::UserWarning")
+    assert result.ret == 0, str(result.stdout)
+
+    shutil.copy(MOCK_PROJECT / "pytest.ini", pytester.path)
+    result = pytester.runpytest('-v', "-W", "error::UserWarning")
+    assert result.ret == 0, str(result.stdout)
+    result.stdout.fnmatch_lines([
+        "*test_first*", "*test_second*", "*test_third*", "*test_fourth*",
+        "*test_fifth*"
+    ])
+
+    shutil.copy(MOCK_PROJECT / "degenerate-pytest.ini",
+                pytester.path / "pytest.ini")
+    result = pytester.runpytest('-v', "-W", "error::UserWarning")
+    assert result.ret != 0, str(result.stdout)
+    result.stderr.fnmatch_lines(
+        ['*file "tests/test_fifth.py" does * the "order" section*'])
